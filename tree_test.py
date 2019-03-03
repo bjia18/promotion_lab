235ff30edd7144ba604a79a3e4f7a0b528566fbf
bjia18 <bjia18@simons-rock.edu>
2019-03-03 13:31:24 -0500

    testing

--- tree_test.py
+++ tree_test.py
@@ -1,5 +1,4 @@
 import dtree_build
-import regression_tree
 import sys
 import csv
 
@@ -14,21 +13,26 @@ def main(col_names=None):
     csv_file_name = sys.argv[1]
 
     data = []
+    labels=True
     with open(csv_file_name) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
-            data.append(list(row))
+            if labels:
+               col_names=row
+               labels=False
+            else:
+                data.append(list(row))
 
     print("Total number of records = ",len(data))
-    tree = regression_tree.buildtree(data, min_gain =0.01, min_samples = 5)
-    regression_tree.printtree(tree, '', col_names)
+    tree = dtree_build.buildtree(data, min_gain =0.01, min_samples = 5)
+    dtree_build.printtree(tree, '', col_names)
 
-    max_tree_depth = regression_tree.max_depth(tree)
+    max_tree_depth = dtree_build.max_depth(tree)
     print("max number of questions=" + str(max_tree_depth))
 
     if len(sys.argv) > 2: # draw option specified
-        import regtree_draw
-        regtree_draw.drawtree(tree, jpeg=csv_file_name+'.jpg')
+        import dtree_draw
+        dtree_draw.drawtree(tree, jpeg=csv_file_name+'.jpg')
 
 if __name__ == "__main__":
     col_names = []
